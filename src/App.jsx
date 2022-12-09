223b5637865d38b3048aee44ee7a3a1b267aa107
Doston <polatovdoston1002@gmail.com>
2022-12-09 11:51:49 +0500

    dom qo'shish

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Type from "./pages/type/Type";
 import ShartnomaView from "./pages/shartnoma/ShartnomaView";
 import CreateObject from "./pages/object/CreateObject";
 import TypeObekt from './pages/type/TypeObekt';
+import DomObject from "./pages/dom/DomObject";
 function App() {
   return (
     <div className="App">
@@ -21,6 +22,7 @@ function App() {
           <Route path="/shartnoma-view" element={<ShartnomaView />} />
           <Route path="/create-object" element={<CreateObject />} />
           <Route path="/type-object" element={<TypeObekt />} />
+          <Route path="/object-dom" element={<DomObject />} />
         </Route>
       </Routes>
     </div>
