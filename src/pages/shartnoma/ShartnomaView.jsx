bc9d754131e53994e28eea2d0d3988e5f6c4cbd2
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2022-12-15 02:18:34 +0500

    text styles fixed

--- src/pages/shartnoma/ShartnomaView.jsx
+++ src/pages/shartnoma/ShartnomaView.jsx
@@ -1,13 +1,61 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { PatternFormat } from "react-number-format";
 import { Form, Input, Button, Checkbox, InputNumber } from "antd";
-import Pdf from "react-to-pdf";
+import {jsPDF} from "jspdf";
 
-import '../shartnoma/shartnoma.css'
-
+import "../shartnoma/shartnoma.css";
+import Test2 from "../../components/Test2";
 
 const ShartnomaView = () => {
-  const ref = useRef()
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleGeneratePdf = () => {
+    const doc = new jsPDF({
+      format: "a3",
+      unit: "px",
+    });
+
+    // Adding the fonts.
+    doc.setFont("Inter-Regular", "normal");
+    doc.html(ref.current, {
+      async callback(doc) {
+        await doc.save("document");
+      },
+    });
+  };
+
+  // const print = () => {
+  //   let elem = document.getElementById("toPdf");
+  //   elem.scrollIntoView();
+  //   h2c(elem).then(canvas => {
+  //     //document.body.appendChild(canvas)
+  //     const img = canvas.toDataURL("image/png", 1);
+  //     //console.log(`"data:image/png;base64,${img}"`)
+
+  //     var imgWidth = 150;
+  //     var pageHeight = 200;
+  //     var imgHeight = (canvas.height * imgWidth) / canvas.width;
+  //     var heightLeft = imgHeight;
+
+  //     const pdf = new JSpdf("p", "mm");
+  //     var position = 10;
+
+  //     pdf.addImage(img, "PNG", 0, position, imgWidth, imgHeight, 100);
+  //     heightLeft -= pageHeight;
+
+  //     while (heightLeft >= 0) {
+  //       position = heightLeft - imgHeight;
+  //       pdf.addPage();
+  //       pdf.addImage(img, "PNG", 0, position, imgWidth, imgHeight);
+  //       heightLeft -= pageHeight;
+  //     }
+
+  //     //pdf.addImage(img, 'PNG', 0, 0)
+  //     pdf.save("export.pdf");
+  //   });
+
+  
 
   const onFinish = (values) => {
     console.log("Success:", values);
@@ -32,7 +80,6 @@ const ShartnomaView = () => {
           }}
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
-          
         >
           <div className="NameInfo">
             <Form.Item
@@ -230,11 +277,19 @@ const ShartnomaView = () => {
               Kiritilgan ma'lumotlar haqiqiyligini tasdiqlayman
             </Checkbox>
             <br />
-            <Form.Item>
-              <Button
+            <Form.Item style={{display: "flex"}}>
+              {/* <Button
                 className="typeObject__btn"
                 type="primary"
                 htmlType="submit"
+              >
+                Yuborish
+              </Button> */}
+
+              <Button
+              onClick={() => setIsVisible(!isVisible)}
+                className="typeObject__btn"
+                type="primary"
               >
                 Yuborish
               </Button>
@@ -243,17 +298,33 @@ const ShartnomaView = () => {
         </Form>
       </div>
 
-      <div className="pdf">
-        <Pdf targetRef={ref} filename="code-example.pdf">
-          {({ toPdf }) => <Button onClick={toPdf}>Generate Pdf</Button>}
-        </Pdf>
-        <div ref={ref}>
-          <h1>Hello CodeSandbox</h1>
-          <h2>Start editing to see some happen!</h2>
+      {isVisible && (
+        <div style={{
+          position: "absolute",
+          top:"0px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          height :"400px",
+          width:"800px",
+          overflowY: "scroll",
+          background: "white",
+          fontSize:"15px"
+
+        }}>
+          <div ref={ref} id="toPdf" style={{
+            marginRight:"30px",
+            padding:"2rem",
+            "page-break-after": "always",
+            textAlign: "center",
+          }}>
+            <Test2/>
+          </div>
+          <br />
+          <Button onClick={handleGeneratePdf}>Yukalsh</Button>
         </div>
-      </div>
+      )}
     </div>
   );
-}
+};
 
-export default ShartnomaView
+export default ShartnomaView;
